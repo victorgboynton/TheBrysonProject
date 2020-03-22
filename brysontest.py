6d678d5b594d65e751373aa6720951e2ed3759d4
Bob's Computer <victorgboynton@gmail.com>
2020-03-22 14:39:20 -0600

    um

--- brysontest.py
+++ brysontest.py
@@ -1,17 +1,29 @@
 #quantity = 15
-#price = 11.290
+#price = 11.2
 
-itemComp = open('itemList.txt','r')
-def itemCalculator(quantity,price):
-	total = quantity*price
-	print("Your total is: "+str(total))
+
 
 class items:
 	def __init__(self,itemName,price):
 		itemName = str(self.itemName)
 		price = self.price
 
+itemCatalog = [] 
 
+with open("itemList.txt") as f:
+	x=1
+	for line in f:
+		itemCatalog[x] = line
+		x = x + 1
+
+
+
+
+def itemCalculator(quantity,price):
+	total = quantity*price
+	print("Your total is: "+str(total))
+
+	
 
 
 quant = float(input("Enter quantity: "))
